c6d52dbab2497d90954d9eea72c877b74cf9f93b
nevrome <clemens@nevrome.de>
2018-06-07 21:29:54 +0200

    started to embed cool widgets

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,8 @@ int main(int argc, char* argv[]){
 	if (argc < 2) { return(EXIT_FAILURE); }
  	std::string ideas_file_path = argv[1];
 
+	//// load and prepare stuff ////
+
 	CTML::Document doc;
 	
 	// css
@@ -14,16 +16,41 @@ int main(int argc, char* argv[]){
 	ref_to_css.SetAttribute("rel", "stylesheet");	
 	doc.AddNodeToHead(ref_to_css);
 
-	doc.AddNodeToBody(CTML::Node("a.link").SetContent("Anchor").SetAttribute("href", "http://www.example.com"));
-	
+	// button
 	CTML::Node my_button = CTML::Node("button");
 	my_button.SetAttribute("style", "vertical-align:middle");
 	my_button.SetAttribute("class", "button");
 	my_button.AppendChild(CTML::Node("span").SetContent("Hover"));	
-	doc.AddNodeToBody(my_button);
 	
+	// twitter
+	CTML::Node twitter_script = CTML::Node("script");
+	twitter_script.SetAttribute("async src", "https://platform.twitter.com/widgets.js");
+	twitter_script.SetAttribute("charset", "utf-8");
+	doc.AddNodeToBody(twitter_script);
+	CTML::Node twitter = CTML::Node("a");
+	twitter.SetAttribute("class", "twitter-timeline");
+	twitter.SetAttribute("href", "https://twitter.com/nevromeCS?ref_src=twsrc%5Etfw");
+	twitter.SetAttribute("data-height", "100vh");
+
+	// github
+	CTML::Node github = CTML::Node("iframe");
+	github.SetAttribute("allowtransparency", "true");
+	github.SetAttribute("frameborder", "0");
+	github.SetAttribute("scrolling", "yes");
+	github.SetAttribute("seamless", "seamless");
+	github.SetAttribute("src", "http://colmdoyle.github.io/gh-activity/gh-activity.html?user=nevrome&type=user");
+	github.SetAttribute("width", "100%");
+	github.SetAttribute("height", "100%");
+
+	// actual page structure
 	
-		
+	CTML::Node left = CTML::Node("div").SetAttribute("class", "left"); 
+	left.AppendChild(twitter);
+	CTML::Node middle = CTML::Node("div").SetAttribute("class", "middle"); 
+	middle.AppendChild(github);
+
+	doc.AddNodeToBody(left);
+	doc.AddNodeToBody(middle);
 
 	std::string index_html_path = ideas_file_path + "index.html";
   return doc.WriteToFile(index_html_path, CTML::Readability::MULTILINE);
